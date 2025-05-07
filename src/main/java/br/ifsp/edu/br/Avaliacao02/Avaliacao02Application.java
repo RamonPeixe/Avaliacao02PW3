86047a4c9bce79355bc60809441c841438a04858
RamonPeixe <r-amoncp@hotmail.com>
2025-05-07 11:38:44 -0300

    Adiciona nome da dupla em Avaliacao02Application como pedido.

--- src/main/java/br/ifsp/edu/br/Avaliacao02/Avaliacao02Application.java
+++ src/main/java/br/ifsp/edu/br/Avaliacao02/Avaliacao02Application.java
@@ -3,6 +3,10 @@ package br.ifsp.edu.br.Avaliacao02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Dupla:
+//Filippa Bittencourt - SC3029832
+//Ramon César Peixe - SC3034992
+
 @SpringBootApplication
 public class Avaliacao02Application {
 
